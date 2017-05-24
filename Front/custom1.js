2a141bb09645ea97ac750a06393c03a9f820cede
Santiago Liñán Romero <s.linan10@uniandes.edu.co>
2017-05-24 09:21:28 -0500

    v2

--- Front/custom1.js
+++ Front/custom1.js
@@ -57,10 +57,6 @@ var myG4 = new Chart(g4, {
     datasets: [{
       label: 'apples',
       data: [12, 19, 3, 17, 28, 24, 7],
-      backgroundColor: "rgb(0,122,122)"
-    }, {
-      label: 'oranges',
-      data: [30, 29, 5, 5, 20, 3, 10],
       backgroundColor: "rgb(192,192,192)"
     }]
   }
